fc8ddfcc60bb54e51495aa2d13fc154f2f571bf0
Ikraam <ibndaanis@gmail.com>
2025-03-01 12:26:20 +0300

    added timelock boost enum

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,19 @@ export type Nonce = {
   nonce: string;
   createdAt: number;
 };
+
+export enum TimelockBoost {
+  NoLock = "noLock",
+  OneMonth = "oneMonth",
+  ThreeMonths = "threeMonths",
+  SixMonths = "sixMonths",
+  NineMonths = "nineMonths"
+}
+
+export const TimelockBoostLabels = {
+  [TimelockBoost.NoLock]: "No Lock (1x)",
+  [TimelockBoost.OneMonth]: "1 Month (2x)",
+  [TimelockBoost.ThreeMonths]: "3 Months (3x)",
+  [TimelockBoost.SixMonths]: "6 Months (4x)",
+  [TimelockBoost.NineMonths]: "9 Months (5x)"
+};
