68ab83e3574e51a66e998536699cedd858ce21be
Ikraam <ibndaanis@gmail.com>
2025-03-02 13:57:41 +0300

    adjusted button styling

--- components/add-sol.tsx
+++ components/add-sol.tsx
@@ -75,14 +75,14 @@ export const AddSol = ({
 
   return (
     <div className="items-center justify-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
-      <span className="relative inline-block overflow-hidden rounded-sm p-[1.5px]">
+      <span className="relative inline-block w-full overflow-hidden rounded-sm p-[1.5px]">
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
         <div className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-sm bg-white text-xs font-medium backdrop-blur-3xl dark:bg-gray-950">
           <Button
             onClick={getAirdrop}
             disabled={pending}
             className={cn(
-              "group inline-flex h-[unset] w-full items-center justify-center rounded-sm border-[1px] border-input bg-gradient-to-tr from-zinc-300/20 via-purple-400/30 to-transparent px-4 py-2 text-center text-gray-900 transition-all hover:bg-gradient-to-tr hover:from-zinc-300/30 hover:via-purple-400/40 hover:to-transparent dark:from-zinc-300/5 dark:via-purple-400/20 dark:text-white dark:hover:from-zinc-300/10 dark:hover:via-purple-400/30 sm:w-auto",
+              "group inline-flex h-[unset] w-full items-center justify-center rounded-sm border-[1px] border-input bg-gradient-to-tr from-zinc-300/20 via-purple-400/30 to-transparent px-4 py-2 text-center text-gray-900 transition-all hover:bg-gradient-to-tr hover:from-zinc-300/30 hover:via-purple-400/40 hover:to-transparent dark:from-zinc-300/5 dark:via-purple-400/20 dark:text-white dark:hover:from-zinc-300/10 dark:hover:via-purple-400/30",
               pending && "opacity-50"
             )}
           >
