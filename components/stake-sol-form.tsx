97d60069c38e3265123ae3fa8a1ebf31a84e50ca
Ikraam <ibndaanis@gmail.com>
2025-03-02 13:24:39 +0300

    removed no lock option

--- components/stake-sol-form.tsx
+++ components/stake-sol-form.tsx
@@ -64,7 +64,7 @@ export function StakeSolForm({ onClose }: { onClose: () => void }) {
     resolver: zodResolver(formSchema),
     defaultValues: {
       amount: "",
-      timelockBoost: TimelockBoost.NoLock
+      timelockBoost: TimelockBoost.OneMonth
     }
   });
 
--- types/index.ts
+++ types/index.ts
@@ -4,7 +4,6 @@ export type Nonce = {
 };
 
 export enum TimelockBoost {
-  NoLock = "noLock",
   OneMonth = "oneMonth",
   ThreeMonths = "threeMonths",
   SixMonths = "sixMonths",
@@ -12,7 +11,6 @@ export enum TimelockBoost {
 }
 
 export const TimelockBoostLabels = {
-  [TimelockBoost.NoLock]: "No Lock (1x)",
   [TimelockBoost.OneMonth]: "1 Month (2x)",
   [TimelockBoost.ThreeMonths]: "3 Months (3x)",
   [TimelockBoost.SixMonths]: "6 Months (4x)",
